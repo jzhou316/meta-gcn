7fba66a66803cff61f94ca2436abdaa7fd6a7104
jzhou316 <jzhou02@g.harvard.edu>
2019-12-03 19:39:26 -0500

    fix the subgraph device inconsistency bug

--- src/gcn_meta/models/gpool_hard_attention.py
+++ src/gcn_meta/models/gpool_hard_attention.py
@@ -5,7 +5,7 @@ from torch.nn.utils.rnn import pad_sequence
 from torch_geometric.nn.inits import glorot, zeros
 from torch_geometric.utils import scatter_
 from torch_scatter import scatter_max
-from torch_geometric.utils import subgraph
+from torch_geometric.utils.num_nodes import maybe_num_nodes
 
 from .common import activation, softmax
 
@@ -19,6 +19,56 @@ def gumbel_samples(base):
     noise.add_(eps).log_().neg_()
     noise.add_(eps).log_().neg_()
     return noise
+
+
+# adapted from torch_geometric.utils.subgraph to fix the GPU device bug
+def subgraph(subset, edge_index, edge_attr=None, relabel_nodes=False,
+             num_nodes=None):
+    r"""Returns the induced subgraph of :obj:`(edge_index, edge_attr)`
+    containing the nodes in :obj:`subset`.
+
+    Args:
+        subset (LongTensor, BoolTensor or [int]): The nodes to keep.
+        edge_index (LongTensor): The edge indices.
+        edge_attr (Tensor, optional): Edge weights or multi-dimensional
+            edge features. (default: :obj:`None`)
+        relabel_nodes (bool, optional): If set to :obj:`True`, the resulting
+            :obj:`edge_index` will be relabeled to hold consecutive indices
+            starting from zero. (default: :obj:`False`)
+        num_nodes (int, optional): The number of nodes, *i.e.*
+            :obj:`max_val + 1` of :attr:`edge_index`. (default: :obj:`None`)
+
+    :rtype: (:class:`LongTensor`, :class:`Tensor`)
+    """
+
+    device = edge_index.device
+
+    if isinstance(subset, list) or isinstance(subset, tuple):
+        subset = torch.tensor(subset, dtype=torch.long)
+
+    if subset.dtype == torch.bool or subset.dtype == torch.uint8:
+        n_mask = subset
+
+        if relabel_nodes:
+            n_idx = torch.zeros(n_mask.size(0), dtype=torch.long, device=device)
+            n_idx[subset] = torch.arange(subset.sum().item(), device=device)
+    else:
+        num_nodes = maybe_num_nodes(edge_index, num_nodes)
+        n_mask = torch.zeros(num_nodes, dtype=torch.bool)
+        n_mask[subset] = 1
+
+        if relabel_nodes:
+            n_idx = torch.zeros(num_nodes, dtype=torch.long, device=device)
+            n_idx[subset] = torch.arange(subset.size(0), device=device)
+
+    mask = n_mask[edge_index[0]] & n_mask[edge_index[1]]
+    edge_index = edge_index[:, mask]
+    edge_attr = edge_attr[mask] if edge_attr is not None else None
+
+    if relabel_nodes:
+        edge_index = n_idx[edge_index]
+
+    return edge_index, edge_attr
 
 
 def get_edge_select(x, edge_index, alpha, training=True, p_keep=None):
